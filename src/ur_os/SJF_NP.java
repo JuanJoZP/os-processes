fc2a1665b33507e053ebf4c6929f2df52f5d5e73
juanjozp <juanzpjose136@gmail.com>
2025-02-20 14:46:35 -0500

    SJF_NP implementation

--- src/ur_os/SJF_NP.java
+++ src/ur_os/SJF_NP.java
@@ -18,8 +18,21 @@ public class SJF_NP extends Scheduler{
    
     @Override
     public void getNext(boolean cpuEmpty) {
-       
-        
+       if(!processes.isEmpty() && cpuEmpty) {
+            int shortest_process_index = -1;
+            int shortest_process_size = Integer.MAX_VALUE;
+            for (int i = 0; i < processes.size(); i++) {
+                int process_size = processes.get(i).getRemainingTimeInCurrentBurst();
+                if (process_size <= shortest_process_size) {
+                    shortest_process_index = i;
+                    shortest_process_size = process_size;
+                }
+            }
+            
+            Process next_p = processes.get(shortest_process_index);
+            processes.remove(shortest_process_index);
+            os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, next_p);
+        }
     }
     
     @Override
