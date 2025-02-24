4d27a6ca1c78e3eb3594babafdda72adaec0a9e9
Serg0casas <sergiocasas@SERGIOs-MacBook-Air.local>
2025-02-24 13:32:04 -0500

    Round Robin

--- src/ur_os/RoundRobin.java
+++ src/ur_os/RoundRobin.java
@@ -1,53 +1,70 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ur_os;
 
 /**
  *
  * @author prestamour
  */
-public class RoundRobin extends Scheduler{
+public class RoundRobin extends Scheduler {
 
     int q;
     int cont;
     boolean multiqueue;
-    
-    RoundRobin(OS os){
+
+    // Constructor con valores por defecto
+    RoundRobin(OS os) {
         super(os);
-        q = 5;
-        cont=0;
-    }
-    
-    RoundRobin(OS os, int q){
-        this(os);
-        this.q = q;
+        this.q = 5;
+        this.cont = 0;
+        this.multiqueue = false;
     }
 
-    RoundRobin(OS os, int q, boolean multiqueue){
-        this(os);
+    // Constructor con quantum personalizado
+    RoundRobin(OS os, int q) {
+        super(os);
         this.q = q;
+        this.cont = 0;
+        this.multiqueue = false;
+    }
+
+    // Constructor con quantum y soporte para multiqueue
+    RoundRobin(OS os, int q, boolean multiqueue) {
+        super(os);
+        this.q = q;
+        this.cont = 0;
         this.multiqueue = multiqueue;
     }
-    
 
-    
-    void resetCounter(){
-        cont=0;
+    // Método para reiniciar el contador
+    void resetCounter() {
+        cont = 0;
     }
-   
+
     @Override
     public void getNext(boolean cpuEmpty) {
-        
-        
+        if (!cpuEmpty) {
+            cont += 1;
+            if (cont > q) {
+                Process p = null;
+                if (!processes.isEmpty()) {
+                    p = processes.remove();
+                }
+                os.interrupt(InterruptType.SCHEDULER_CPU_TO_RQ, p);
+                cont = 1;
+            }
+        } else if (!processes.isEmpty()) {
+            Process p = processes.remove();
+            os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, p);
+            cont = 1;
+        }
     }
-    
-    
-    @Override
-    public void newProcess(boolean cpuEmpty) {} //Non-preemtive in this event
 
     @Override
-    public void IOReturningProcess(boolean cpuEmpty) {} //Non-preemtive in this event
-    
+    public void newProcess(boolean cpuEmpty) {
+        // Non-preemptive in this event
+    }
+
+    @Override
+    public void IOReturningProcess(boolean cpuEmpty) {
+        // Non-preemptive in this event
+    }
 }
